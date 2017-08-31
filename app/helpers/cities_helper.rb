a8eeab687f7d4e44344267166102314c66c6a155
Alexander Puentes <Alexander.puen91@gmail.com>
2017-08-31 12:09:01 -0400

    HTTParty

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,5 +1,5 @@
 module CitiesHelper
-  require 'Httparty'
+  require 'HTTParty'
   def get_weather(city)
     response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{city}&units=imperial&appid=#{ENV['OPENWEATHER_KEY']}")
     return response.parsed_response
